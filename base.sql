0efa723287c006299bff1efcb07ba71f4b12ad05
henryimb <henryimbaquingo23@gmail.com>
2019-02-08 21:46:49 -0500

    se a insertado un campo

--- base.sql
+++ base.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE base;
 USE base;
 CREATE TABLE usuario(id VARCHAR (45) PRIMARY KEY NOT NULL AUTO_ICREMENT , nombre VARCHAR(45), pasword VARCHAR (45));
-
+insert into usuario values ('1','henry','imb');
